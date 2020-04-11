c023eda2b6fa13dbb6a514a4a1661d686d581e02
Mahela-DESKTOP <mahela37@gmail.com>
2020-04-10 20:04:42 -0700

    finished html parser

--- html_parser.py
+++ html_parser.py
@@ -1,11 +1,28 @@
-from bs4 import BeautifulSoup
+def parseMainPage():
+    from bs4 import BeautifulSoup
+    import re
+    import requests
 
-f = open("html_source.txt", "r")
-f_contents=f.read()
-f.close()
+    r=requests.get("https://www.clg.org/Class-Action/List-of-Class-Actions")
+    soup=BeautifulSoup(r.text,'html.parser')
+    mydivs=soup.find("div",{"class":'classactions'})
 
+    div_array=[]
+    category=""
+    for div in mydivs:
+        try:
+            class_type=div.attrs['class'][0]+div.attrs['class'][1]  #it's a lawsuit entry
+            if(class_type=="actionwith_image"):
+                #the regex is for french characters that the website has used for a few entries. We wanna strip ito out.
+                #for each lawsuit, make a dict entry with the name, href and the category
+                temp_entry={'name':re.sub(r'[^a-zA-Z]', "", div.contents[5].contents[0].contents[0].strip()),'href':div.contents[5].contents[0].attrs['href'],'category':category}
+                div_array.append(temp_entry)
 
-soup=BeautifulSoup(f_contents,'html.parser')
+        except:
+            try:
+                if('cat' in class_type):    #it's a new category
+                    category=class_type
+            except:
+                """"""
 
-mydivs=soup.findAll("div", {"class": "label"})
-print(mydivs)
+    return div_array
--- main.py
+++ main.py
@@ -1,19 +1,44 @@
 import time
 from selenium import webdriver
-#testing
-print ("feelsbadman.jpg")
 
 
 
-driver = webdriver.Chrome('chromedriver.exe')  # Optional argument, if not specified will search path.
-driver.get('https://www.clg.org/Class-Action/List-of-Class-Actions');
-time.sleep(5) # Let the user actually see something!
-html_source=driver.page_source
+#
+# driver = webdriver.Chrome('chromedriver.exe')  # Optional argument, if not specified will search path.
+# driver.get('https://www.clg.org/Class-Action/List-of-Class-Actions');
+# time.sleep(5) # Let the user actually see something!
+# html_source=driver.page_source
+#
+#
+# f= open("html_source.txt","w")
+# f.write(html_source)
+# f.close()
+#
+#
+# driver.quit()
 
 
-f= open("html_source.txt","w")
-f.write(html_source)
+import html_parser
+
+
+f= open("entries_completed.txt","r")
+completedList=f.readlines()
 f.close()
 
+lawsuitList=html_parser.parseMainPage()
+newLawsuitsList=[]
+for lawsuit in lawsuitList:
+    alreadyDone=0
+    lawsuitName=lawsuit['name']
+    for completedEntry in completedList:
+        completedEntry=completedEntry.strip()
+        completedEntry=completedEntry.split(";")
+        if(lawsuitName==completedEntry[0]):
+            alreadyDone=1
+            break
+    if(alreadyDone==0):
+        newLawsuitsList.append(lawsuit)
 
-driver.quit()
+
+for lawsuit in newLawsuitsList:
+    print(lawsuit)
